e890b718eae0cdae63ea48c15201e96a3aa4e079
yinka <holayinkajr@gmail.com>
2021-09-15 02:59:18 +0100

    first commit

---
+++ learning_logs/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Topic(models.Model):
+
+    """A topic the user is learning about """
+    text = models.CharField(max_length=50)
+    date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text
+
+class Entry(models.Model):
+    """Something specific learned about a topic."""
+
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added =  models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        if(len(self.text) < 50):
+            return self.text
+        return f"{self.text[:50]}..."
